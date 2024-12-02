6e4d585abd0788f8386323f8b02ec3d9521feaf2
cyril0124 <211998078@qq.com>
2024-12-02 22:01:30 +0800

    feat(lua_scoreboard): export all design parameters as enviroment variable

--- src/test/scala/lntest/top/LuaScoreboard.scala
+++ src/test/scala/lntest/top/LuaScoreboard.scala
@@ -33,9 +33,34 @@ class LuaScoreboard(nrL2:Int, nrL2Bank:Int, nrPcu:Int, nrDcu:Int) extends BlackB
        |  import "DPI-C" function void verilua_final();
        |  import "DPI-C" function void verilua_main_step_safe();
        |`ifdef VERILATOR
-       |  initial verilua_init();
+       |  initial begin
+       |    $$c("char value[50];");
+       |
+       |    $$c("sprintf(value, \\"%d\\",", NR_L2, ");");
+       |    $$c("setenv(\\"NR_L2\\", value, 1);");
+       |
+       |    $$c("sprintf(value, \\"%d\\",", NR_L2_BANK, ");");
+       |    $$c("setenv(\\"NR_L2_BANK\\", value, 1);");
+       |
+       |    $$c("sprintf(value, \\"%d\\",", NR_PCU, ");");
+       |    $$c("setenv(\\"NR_PCU\\", value, 1);");
+       |
+       |    $$c("sprintf(value, \\"%d\\",", NR_DCU, ");");
+       |    $$c("setenv(\\"NR_DCU\\", value, 1);");
+       |
+       |    verilua_init();
+       |  end
        |`else
-       |  initial #1 verilua_init();
+       |  import "DPI-C" function void setenv(string name, string value, integer replace);
+       |
+       |  initial begin 
+       |    setenv("NR_L2", $$sformatf("%d", NR_L2), 1);
+       |    setenv("NR_L2_BANK", $$sformatf("%d", NR_L2_BANK), 1);
+       |    setenv("NR_PCU", $$sformatf("%d", NR_PCU), 1);
+       |    setenv("NR_DCU", $$sformatf("%d", NR_DCU), 1);
+       |
+       |    #1 verilua_init();
+       |  end
        |`endif
        |
        |always @ (negedge clock) begin
