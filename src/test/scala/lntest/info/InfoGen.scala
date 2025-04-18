30487796e81cf76f406621bbee368de9b5a5a762
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-04-18 15:19:13 +0800

    fix: set l2cache nodeId correctly

--- src/test/scala/lntest/info/InfoGen.scala
+++ src/test/scala/lntest/info/InfoGen.scala
@@ -17,7 +17,7 @@ object InfoGen {
     val socPath = soc.get.pathName
     val zjInfo = ZhujiangGlobal.getRingDesc(zjP.ciName)
     val ccns = zjP.island.filter(_.nodeType == NodeType.CC)
-    val ccnStrSeq = ccns.map(n => s"{\"$socPath.cc_${n.domainId}.tile.l2cache\", {0x${n.nodeId.toHexString}}},\n")
+    val ccnStrSeq = ccns.map(n => s"{\"$socPath.cc_${n.domainId}.tile.l2cache\", {0x${(n.nodeId + 1).toHexString}}},\n")
     val l2cStrSeq = s"l2c = {\n" +: ccnStrSeq.map(b => s"  $b") :+ "},\n"
     val result = s"soc = {\n" +: (zjInfo ++ l2cStrSeq).map(b => s"  $b") :+ "}\n"
     FileRegisters.add("generated-src", "soc.lua", result.reduce(_ ++ _), dontCarePrefix = true)
