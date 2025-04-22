fee98ee30c1aa6c2ea1715ca5c6611043568f7a9
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-04-22 17:37:17 +0800

    fix: use nact minimal config

--- src/main/scala/linknan/generator/MinimalNanhuConfig.scala
+++ src/main/scala/linknan/generator/MinimalNanhuConfig.scala
@@ -2,153 +2,61 @@ package linknan.generator
 
 import org.chipsalliance.cde.config.Config
 import xiangshan.XSCoreParamsKey
-import xiangshan.backend.dispatch.DispatchParameters
-import xiangshan.backend.regfile.{IntPregParams, VfPregParams}
+import xiangshan.backend.regfile.{IntPregParams, V0PregParams, VfPregParams, VlPregParams}
 import xiangshan.cache.DCacheParameters
-import xiangshan.cache.mmu.{L2TLBParameters, TLBParameters}
 import xiangshan.frontend.icache.ICacheParameters
 
 class MinimalNanhuConfig extends Config((site, here, up) => {
   case XSCoreParamsKey => up(XSCoreParamsKey).copy(
-    DecodeWidth = 6,
-    RenameWidth = 6,
-    RobCommitWidth = 8,
-    FetchWidth = 4,
-    VirtualLoadQueueSize = 24,
-    LoadQueueRAWSize = 12,
-    LoadQueueReplaySize = 24,
-    LoadUncacheBufferSize = 8,
-    LoadQueueNWriteBanks = 4, // NOTE: make sure that LoadQueue{RAR, RAW, Replay}Size is divided by LoadQueueNWriteBanks.
-    RollbackGroupSize = 8,
-    StoreQueueSize = 20,
-    StoreQueueNWriteBanks = 4, // NOTE: make sure that StoreQueueSize is divided by StoreQueueNWriteBanks
-    StoreQueueForwardWithMask = true,
-    // ============ VLSU ============
-    VlMergeBufferSize = 16,
-    VsMergeBufferSize = 8,
-    UopWritebackWidth = 2,
-    // ==============================
-    RobSize = 48,
-    RabSize = 96,
-    FtqSize = 8,
-    IBufSize = 24,
-    IBufNBank = 6,
-    StoreBufferSize = 4,
-    StoreBufferThreshold = 3,
-    dpParams = DispatchParameters(
-      IntDqSize = 12,
-      FpDqSize = 12,
-      LsDqSize = 12,
-      IntDqDeqWidth = 8,
-      FpDqDeqWidth = 6,
-      VecDqDeqWidth = 6,
-      LsDqDeqWidth = 6
-    ),
+    //Frontend
+    IBufSize = 32, //32
+    FtqSize = 32, //8
+    //Backend
+    RobSize = 72, //96
+    RabSize = 96, //96
     intPreg = IntPregParams(
-      numEntries = 64,
+      numEntries = 64,  //128
       numRead = None,
       numWrite = None,
     ),
     vfPreg = VfPregParams(
-      numEntries = 128,
+      numEntries = 120,  //160
       numRead = None,
       numWrite = None,
     ),
+    v0Preg = V0PregParams(
+      numEntries = 8, //22
+      numRead = None,
+      numWrite = None,
+    ),
+    vlPreg = VlPregParams(
+      numEntries = 8, //32
+      numRead = None,
+      numWrite = None,
+    ),
+    //Memblock
+    VirtualLoadQueueSize = 24,    //56
+    LoadQueueRAWSize = 12,        //24
+    LoadQueueReplaySize = 12,     //32
+    LoadUncacheBufferSize = 4,    //8
+    StoreQueueSize = 16,          //32
+    StoreBufferSize = 4,          //8
+    StoreBufferThreshold = 3,     //7
+    VlMergeBufferSize = 4,        //16
+    VsMergeBufferSize = 4,        //16
+    VSegmentBufferSize = 4,       //8
+    //L1 i&d
     icacheParameters = ICacheParameters(
-      nSets = 64, // 16KB ICache
-      tagECC = Some("parity"),
-      dataECC = Some("parity"),
-      replacer = Some("setplru"),
+      nSets = 128, //32 kB ICache, 16*1024/4/64
+      nWays = 2,
     ),
     dcacheParametersOpt = Some(DCacheParameters(
-      nSets = 256, // 2 bits alias
+      nSets = 128, //32 kB DCache, 16*1024/4/64
       nWays = 2,
-      tagECC = Some("secded"),
-      dataECC = Some("secded"),
-      replacer = Some("setplru"),
-      nMissEntries = 4,
-      nProbeEntries = 4,
-      nReleaseEntries = 4,
-      nMaxPrefetchEntry = 2,
-    )),
-    // ============ BPU ===============
-    EnableGHistDiff = false,
-    FtbSize = 256,
-    FtbWays = 2,
-    RasSize = 8,
-    RasSpecSize = 16,
-    TageTableInfos =
-      Seq((512, 4, 6),
-        (512, 9, 6),
-        (1024, 19, 6)),
-    SCNRows = 128,
-    SCNTables = 2,
-    SCHistLens = Seq(0, 5),
-    ITTageTableInfos =
-      Seq((256, 4, 7),
-        (256, 8, 7),
-        (512, 16, 7)),
-    // ================================
-    itlbParameters = TLBParameters(
-      name = "itlb",
-      fetchi = true,
-      useDmode = false,
-      NWays = 4,
-    ),
-    dtlbParameters = TLBParameters(
-      name = "ldtlb",
-      NWays = 4,
-      partialStaticPMP = true,
-      outsideRecvFlush = true,
-      outReplace = false,
-      lgMaxSize = 4
-    ),
-    ldtlbParameters = TLBParameters(
-      name = "ldtlb",
-      NWays = 4,
-      partialStaticPMP = true,
-      outsideRecvFlush = true,
-      outReplace = false,
-      lgMaxSize = 4
-    ),
-    sttlbParameters = TLBParameters(
-      name = "sttlb",
-      NWays = 4,
-      partialStaticPMP = true,
-      outsideRecvFlush = true,
-      outReplace = false,
-      lgMaxSize = 4
-    ),
-    hytlbParameters = TLBParameters(
-      name = "hytlb",
-      NWays = 4,
-      partialStaticPMP = true,
-      outsideRecvFlush = true,
-      outReplace = false,
-      lgMaxSize = 4
-    ),
-    pftlbParameters = TLBParameters(
-      name = "pftlb",
-      NWays = 4,
-      partialStaticPMP = true,
-      outsideRecvFlush = true,
-      outReplace = false,
-      lgMaxSize = 4
-    ),
-    btlbParameters = TLBParameters(
-      name = "btlb",
-      NWays = 4,
-    ),
-    l2tlbParameters = L2TLBParameters(
-      l3Size = 4,
-      l2Size = 4,
-      l1nSets = 4,
-      l1nWays = 4,
-      l1ReservedBits = 1,
-      l0nSets = 4,
-      l0nWays = 8,
-      l0ReservedBits = 0,
-      spSize = 4,
-    )
+      nMissEntries = 4,     //16
+      nProbeEntries = 2,    //4
+      nReleaseEntries = 2,  //4
+      nMaxPrefetchEntry = 2,//6
+    ))
   )
 })
