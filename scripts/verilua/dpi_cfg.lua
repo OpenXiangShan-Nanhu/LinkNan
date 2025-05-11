70ed2740e1f64bb90424157c1705a8995e12bd4f
cyril0124 <211998078@qq.com>
2025-05-11 15:18:56 +0800

    feat(lua_scb): update dpi_cfg.lua

--- scripts/verilua/dpi_cfg.lua
+++ scripts/verilua/dpi_cfg.lua
@@ -1,28 +1,44 @@
-dpi_exporter_config = {
-    {
-        module = "SimTop",
-        signals = { "difftest_timer" },
-        is_top_module = true
-    },
+add_pattern {
+    module = "SimTop",
+    signals = "difftest_timer"
+}
 
-    {
-        module = "TL2CHICoupledL2",
-        signals = {
-            "clock",
-            -- TODO: consider MMIOBridge
-            "auto_in_.*(valid|ready|bits_address|bits_opcode|bits_param|bits_source|bits_data|bits_sink)",
-            "io_chi.*_(valid|ready|bits_addr|bits_opcode|bits_size|bits_txnID|bits_srcID|bits_tgtID|bits_dataID|bits_dbID|bits_resp|bits_data|bits_be|bits_homeNID|bits_retToSrc|bits_fwdState|bits_fwdTxnID|bits_fwdNID)"
-        }
-    },
+add_pattern {
+    module = "TL2CHICoupledL2",
+    signals = "clock"
+}
 
-    {
-        module = "AxiBridge",
-        signals = {
-            "axi_aw_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)",
-            "axi_ar_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)",
-            "axi_w_.*(valid|ready|bits_data|bits_strb|bits_last)",
-            "axi_b_.*(valid|ready|bits_id|bits_resp)",
-            "axi_r_.*(valid|ready|bits_id|bits_data|bits_resp|bits_last)"
-        }
-    }
+add_pattern {
+    module = "TL2CHICoupledL2",
+    signals = "auto_in_.*(valid|ready|bits_address|bits_opcode|bits_param|bits_source|bits_data|bits_sink)"
+}
+
+add_pattern {
+    module = "TL2CHICoupledL2",
+    signals = "io_chi.*_(valid|ready|bits_addr|bits_opcode|bits_size|bits_txnID|bits_srcID|bits_tgtID|bits_dataID|bits_dbID|bits_resp|bits_data|bits_be|bits_homeNID|bits_retToSrc|bits_fwdState|bits_fwdTxnID|bits_fwdNID)"
+}
+
+add_pattern {
+    module = "AxiBridge",
+    signals = "axi_aw_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)"
+}
+
+add_pattern {
+    module = "AxiBridge",
+    signals = "axi_ar_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)"
+}
+
+add_pattern {
+    module = "AxiBridge",
+    signals = "axi_w_.*(valid|ready|bits_data|bits_strb|bits_last)"
+}
+
+add_pattern {
+    module = "AxiBridge",
+    signals = "axi_b_.*(valid|ready|bits_id|bits_resp)"
+}
+
+add_pattern {
+    module = "AxiBridge",
+    signals = "axi_r_.*(valid|ready|bits_id|bits_data|bits_resp|bits_last)"
 }
