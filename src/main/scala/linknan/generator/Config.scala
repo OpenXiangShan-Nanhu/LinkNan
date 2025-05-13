89d92d9f90a9132750a8acfb2094b1a370f68fb8
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-05-13 17:57:36 +0800

    chore: reduce minimal config

--- src/main/scala/linknan/generator/Config.scala
+++ src/main/scala/linknan/generator/Config.scala
@@ -141,15 +141,11 @@ class MinimalNocConfig(socket: String) extends Config((site, here, up) => {
     nodeParams = Seq(
       NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 0),
       NodeParam(nodeType = NodeType.CC, socket = socket),
+      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(1, 32, "memsys"))),
       NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 0),
 
       NodeParam(nodeType = NodeType.RI, axiDevParams = Some(AxiDeviceParams(0, 32, "default", "main", Some(AxiParams(idBits = 14))))),
       NodeParam(nodeType = NodeType.HI, axiDevParams = Some(AxiDeviceParams(0, 32, "default", "main")), defaultHni = true),
-
-      NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 1),
-      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(1, 32, "default"))),
-      NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 1),
-
       NodeParam(nodeType = NodeType.M),
       NodeParam(nodeType = NodeType.P)
     )
