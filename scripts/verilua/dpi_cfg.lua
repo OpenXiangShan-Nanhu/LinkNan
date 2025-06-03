7b405693ef06e17f6d9506dd34285c47f02e3283
cyril0124 <211998078@qq.com>
2025-06-03 10:46:27 +0800

    feat(lua_scb): update dpi_cfg.lua

--- scripts/verilua/dpi_cfg.lua
+++ scripts/verilua/dpi_cfg.lua
@@ -8,45 +8,37 @@ add_pattern {
     signals = "clock"
 }
 
-add_pattern {
-    module = "TL2CHICoupledL2",
-    signals = "auto_in_.*(valid|ready|bits_address|bits_opcode|bits_param|bits_source|bits_data|bits_sink)"
-}
+for _, chnl in ipairs({ "a", "b", "c", "d", "e" }) do
+    add_pattern {
+        module = "TL2CHICoupledL2",
+        sensitive_signals = ".*valid",
+        signals = "auto_in_.*_" .. chnl .. "_(valid|ready|bits_address|bits_opcode|bits_param|bits_source|bits_data|bits_sink)"
+    }
+end
 
-add_pattern {
-    module = "TL2CHICoupledL2",
-    signals = "io_chi.*_(valid|ready|bits_addr|bits_opcode|bits_size|bits_txnID|bits_srcID|bits_tgtID|bits_dataID|bits_dbID|bits_resp|bits_data|bits_be|bits_homeNID|bits_retToSrc|bits_fwdState|bits_fwdTxnID|bits_fwdNID)"
-}
+for _, chnl in ipairs({ "tx_req", "tx_rsp", "tx_dat", "rx_snp", "rx_rsp", "rx_dat" }) do
+    add_pattern {
+        module = "TL2CHICoupledL2",
+        sensitive_signals = ".*valid",
+        signals = "io_chi_" .. chnl .. "_(valid|ready|bits_addr|bits_opcode|bits_size|bits_txnID|bits_srcID|bits_tgtID|bits_dataID|bits_dbID|bits_resp|bits_data|bits_be|bits_homeNID|bits_retToSrc|bits_fwdState|bits_fwdTxnID|bits_fwdNID)"
+    }
+end
 
-add_pattern {
-    module = "AxiBridge",
-    signals = "axi_aw_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)"
-}
+for _, chnl in ipairs({ "aw", "ar", "w", "b", "r" }) do
+    add_pattern {
+        module = "AxiBridge",
+        sensitive_signals = ".*valid",
+        signals = "axi_" .. chnl .. "_(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache|bits_data|bits_strb|bits_last|bits_resp)"
+    }
+end
 
-add_pattern {
-    module = "AxiBridge",
-    signals = "axi_ar_.*(valid|ready|bits_id|bits_addr|bits_len|bits_size|bits_burst|bits_cache)"
-}
-
-add_pattern {
-    module = "AxiBridge",
-    signals = "axi_w_.*(valid|ready|bits_data|bits_strb|bits_last)"
-}
-
-add_pattern {
-    module = "AxiBridge",
-    signals = "axi_b_.*(valid|ready|bits_id|bits_resp)"
-}
-
-add_pattern {
-    module = "AxiBridge",
-    signals = "axi_r_.*(valid|ready|bits_id|bits_data|bits_resp|bits_last)"
-}
-
-add_pattern {
-    module = "DongJiang",
-    signals = "io_lan_(tx_req|tx_resp|tx_data|tx_snoop|rx_req|rx_resp|rx_data)(?!.*(Excl|RespErr|CBusy|DoNotGoToSD|.*Attr)).*"
-}
+for _, chnl in ipairs({ "tx_req", "tx_resp", "tx_data", "tx_snoop", "rx_req", "rx_resp", "rx_data" }) do
+    add_pattern {
+        module = "DongJiang",
+        sensitive_signals = ".*valid",
+        signals = "io_lan_" .. chnl .. "(?!.*(Excl|RespErr|CBusy|DoNotGoToSD|.*Attr)).*"
+    }
+end
 
 add_pattern {
     module = "HomeWrapper",
