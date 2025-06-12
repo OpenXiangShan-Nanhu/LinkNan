fd9422d080deb28437fbd5645806a26a27bc17c5
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-06-12 21:32:13 +0800

    feat: separate fpga config from minimal config

--- src/main/scala/linknan/generator/Config.scala
+++ src/main/scala/linknan/generator/Config.scala
@@ -124,7 +124,7 @@ class ReducedNocConfig(socket: String) extends Config((site, here, up) => {
   )
 })
 
-class MinimalNocConfig(socket: String, ddrBuf:Int) extends Config((site, here, up) => {
+class MinimalNocConfig(socket: String) extends Config((site, here, up) => {
   case ZJParametersKey => ZJParameters(
     hnxBankOff = AddrConfig.interleaveOffset,
     nodeParams = Seq(
@@ -137,7 +137,7 @@ class MinimalNocConfig(socket: String, ddrBuf:Int) extends Config((site, here, u
       NodeParam(nodeType = NodeType.HI, axiDevParams = Some(AxiDeviceParams(0, 8, "default", "main")), defaultHni = true),
 
       NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 1),
-      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(ddrBuf, 32, "memsys"))),
+      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(1, 32, "memsys"))),
       NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 1),
 
       NodeParam(nodeType = NodeType.M),
@@ -165,6 +165,27 @@ class ExtremeNocConfig(socket: String) extends Config((site, here, up) => {
 
       NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(1, 32, "default"))),
       NodeParam(nodeType = NodeType.M),
+    )
+  )
+})
+
+class FpgaNocConfig(socket: String, ddrBuf:Int) extends Config((site, here, up) => {
+  case ZJParametersKey => ZJParameters(
+    hnxBankOff = AddrConfig.interleaveOffset,
+    nodeParams = Seq(
+      NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 0),
+      NodeParam(nodeType = NodeType.CC, socket = socket),
+      NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 0),
+
+      NodeParam(nodeType = NodeType.RH, axiDevParams = Some(AxiDeviceParams(0, 16, "default", "main", Some(AxiParams(idBits = 13))))),
+      NodeParam(nodeType = NodeType.HI, axiDevParams = Some(AxiDeviceParams(0, 8, "default", "main")), defaultHni = true),
+
+      NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 1),
+      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(ddrBuf, 32, "memsys", "0"))),
+      NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 1),
+
+      NodeParam(nodeType = NodeType.M),
+      NodeParam(nodeType = NodeType.P)
     )
   )
 })
@@ -277,12 +298,12 @@ object ConfigGenerater {
     val nocCfg = noc match {
       case "full" => new FullNocConfig(socket)
       case "medium" => new ReducedNocConfig(socket)
-      case "small" => new MinimalNocConfig(socket, 0)
-      case "fpga" => new MinimalNocConfig(socket, 32)
+      case "small" => new MinimalNocConfig(socket)
+      case "fpga" => new FpgaNocConfig(socket, 32)
       case "extreme" => new ExtremeNocConfig(socket)
       case _ =>
         require(requirement = false, s"not supported noc config: $noc")
-        new MinimalNocConfig(socket, 0)
+        new MinimalNocConfig(socket)
     }
     new Config(
       coreCfg ++ l3Cfg ++ nocCfg ++ new BaseConfig
