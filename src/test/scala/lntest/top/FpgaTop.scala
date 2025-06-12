7a418a3634bd6ff49604145e8246729d669f4041
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-06-12 15:17:51 +0800

    feat(fpga_top): let reset be negative active

--- src/test/scala/lntest/top/FpgaTop.scala
+++ src/test/scala/lntest/top/FpgaTop.scala
@@ -6,30 +6,32 @@ import linknan.generator.Generator
 import linknan.soc.{LNTop, LinkNanParamsKey}
 import org.chipsalliance.cde.config.Parameters
 import org.chipsalliance.diplomacy.DisableMonitors
-import xs.utils.FileRegisters
+import xs.utils.{FileRegisters, ResetGen}
 import xs.utils.perf.DebugOptionsKey
 import xs.utils.stage.XsStage
 import zhujiang.{NocIOHelper, ZJParametersKey}
 import zhujiang.axi.AxiUtils
 
-class FpgaTop(implicit val p: Parameters) extends Module with NocIOHelper {
+class FpgaTop(implicit val p: Parameters) extends RawModule with NocIOHelper {
   override val desiredName = "XlnFpgaTop"
-  override def resetType = Module.ResetType.Asynchronous
   private val soc = Module(new LNTop)
   val io = IO(new Bundle {
+    val resetn = Input(Bool())
     val core_clk = Input(Vec(soc.io.cluster_clocks.size, Clock()))
     val noc_clk = Input(Clock())
     val rtc_clk = Input(Bool())
     val reset_vector = Input(UInt(soc.io.default_reset_vector.getWidth.W))
     val ext_intr = Input(UInt(soc.io.ext_intr.getWidth.W))
   })
+  private val _reset = (!io.resetn).asAsyncReset
+  private val resetSync = withClockAndReset(io.noc_clk, _reset) { ResetGen(2, None) }
 
   soc.io.cluster_clocks := io.core_clk
   soc.io.noc_clock := io.noc_clk
   soc.io.rtc_clock := io.rtc_clk
   soc.io.ext_intr := io.ext_intr
   soc.io.default_reset_vector := io.reset_vector
-  soc.io.reset := reset
+  soc.io.reset := resetSync
   soc.io.dft := DontCare
   soc.io.ramctl := DontCare
   soc.io.ci := 0.U
