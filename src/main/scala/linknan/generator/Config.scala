43060dea6713a2626f21d3425dd68059e3bc2966
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-07-17 13:02:54 +0800

    chore(fpga): new single core config

--- src/main/scala/linknan/generator/Config.scala
+++ src/main/scala/linknan/generator/Config.scala
@@ -205,6 +205,34 @@ class FpgaBoscSingleNocConfig(socket: String) extends Config((site, here, up) =>
   )
 })
 
+class FpgaBoscSingleNocV2Config(socket: String) extends Config((site, here, up) => {
+  case ZJParametersKey => ZJParameters(
+    asyncParams = AsyncQueueParams(narrow = true),
+    hnxBankOff = AddrConfig.interleaveOffset,
+    nodeParams = Seq(
+      NodeParam(nodeType = NodeType.CC, socket = socket),
+      NodeParam(nodeType = NodeType.P),
+      NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 0),
+      NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 0),
+      NodeParam(nodeType = NodeType.HF, bankId = 2, hfpId = 0),
+      NodeParam(nodeType = NodeType.HF, bankId = 3, hfpId = 0),
+
+      NodeParam(nodeType = NodeType.P),
+      NodeParam(nodeType = NodeType.S,  axiDevParams = Some(AxiDeviceParams(32, 32, "dev", "mem_0"))),
+      NodeParam(nodeType = NodeType.M,  axiDevParams = Some(AxiDeviceParams(4, 32, "dev"))),
+      NodeParam(nodeType = NodeType.RI, axiDevParams = Some(AxiDeviceParams(4, 16, "dev", "main", Some(AxiParams(idBits = 13))))),
+      NodeParam(nodeType = NodeType.HI, axiDevParams = Some(AxiDeviceParams(4, 8,  "dev", "main")), defaultHni = true),
+      NodeParam(nodeType = NodeType.P),
+
+      NodeParam(nodeType = NodeType.HF, bankId = 3, hfpId = 1),
+      NodeParam(nodeType = NodeType.HF, bankId = 2, hfpId = 1),
+      NodeParam(nodeType = NodeType.HF, bankId = 1, hfpId = 1),
+      NodeParam(nodeType = NodeType.HF, bankId = 0, hfpId = 1),
+      NodeParam(nodeType = NodeType.P)
+    )
+  )
+})
+
 class FpgaBoscQuadNocConfig(socket: String) extends Config((site, here, up) => {
   case ZJParametersKey => ZJParameters(
     asyncParams = AsyncQueueParams(narrow = true),
@@ -350,6 +378,7 @@ object ConfigGenerater {
       case "small" => new MinimalNocConfig(socket)
       case "fpga_inno_1" => new FpgaInnoSingleNocConfig(socket)
       case "fpga_bosc_1" => new FpgaBoscSingleNocConfig(socket)
+      case "fpga_bosc_v2_1" => new FpgaBoscSingleNocV2Config(socket)
       case "fpga_bosc_4" => new FpgaBoscQuadNocConfig(socket)
       case _ =>
         require(requirement = false, s"not supported noc config: $noc")
