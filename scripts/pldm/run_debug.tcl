be45816c49fc726a218f0ebd0630f5031d115032
gmx <guanmingxing@bosc.ac.cn>
2025-10-21 14:33:30 +0800

    fix: update pldm run script

--- scripts/pldm/run_debug.tcl
+++ scripts/pldm/run_debug.tcl
@@ -2,11 +2,11 @@ set DESIGN_NAME "XiangShan-Nanhu"
 
 set DESIGN_DIR $env(PLDM_COMP_DIR)
 
-set CUST_INIT_QEL $env(PLDM_RUN_MEMINIT_SCR)
-
 debug $DESIGN_DIR
 xeset designName $DESIGN_NAME
 host $env(PLDM_HOST)
+
+xc wait
 xc xt0 zt0 on -tbrun -initok
 
 database -open pldm_db
@@ -16,10 +16,13 @@ xeset triggerPos 2
 
 run -swap
 
-if { $CUST_INIT_QEL != "" } { source $CUST_INIT_QEL }
+if {[info exists env(PLDM_RUN_MEMINIT_SCR)]} {
+  source $env(PLDM_RUN_MEMINIT_SCR)
+}
 
 run
 
 database -upload
+
 xc off
 exit
